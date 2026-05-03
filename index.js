9dd28831a00197d08a8f819f71186cfcdd0d83f6
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:38:22 +0200

    Initial commit: index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// euipo-trademark-application - EUIPO trademark application helper - European Union IP Office integration. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
